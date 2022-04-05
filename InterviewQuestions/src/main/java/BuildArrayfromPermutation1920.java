b357db002810f4497a39549568c6a26c679cd3a5
josh <josh@joshuas-mini.lan>
2022-04-04 22:08:40 -0500

    commit build array from permutation

---
+++ InterviewQuestions/src/main/java/BuildArrayfromPermutation1920.java
@@ -0,0 +1,19 @@
+
+public class BuildArrayfromPermutation1920 {
+
+	public static void main(String[] args) {
+		
+		int[] a = {0, 2, 1, 5, 3, 4};
+		
+		buildArray(a);
+
+	}
+	
+	public static int[] buildArray(int[] nums) {
+		
+		
+		
+		return nums;
+	}
+
+}
