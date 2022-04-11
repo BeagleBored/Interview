d81779c0dab312a61e66c4a7b5c3fb3cc36de663
josh <josh@joshuas-mini.lan>
2022-04-11 16:26:39 -0500

    created memory for array

---
+++ InterviewQuestions/src/main/java/ShuffleTheArray1470.java
@@ -0,0 +1,33 @@
+
+public class ShuffleTheArray1470 {
+
+	public static void main(String[] args) {
+		int[] array = {2,5,1,3,4,7};
+		int number = 3;
+
+	}
+	
+	public static int[] shuffle(int[] nums, int n) {
+		// nums[0] = 2 = x1
+		// nums[1] = 5 = x2
+		// nums[2] = 1 = x3
+		// nums[3] = 3 = y1
+		// nums[4] = 4 = y2
+		// nums[5] = 7 = y3
+		
+		//answer = [2,3,5,4,1,7]
+		
+		//Create an array and assign memory to it
+		int[] ans = new int[nums.length]; 
+		
+		for(int i = 0; i<= nums.length/2; i++){
+			
+			
+		}
+		
+		
+		return nums;
+		
+	}
+
+}
