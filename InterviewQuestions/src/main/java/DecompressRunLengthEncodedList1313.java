57e501d2bebc67d43edeabe5e1e251a4fa5924cd
josh <josh@joshuas-mini.lan>
2022-04-19 16:54:11 -0500

    Start of challenge

---
+++ InterviewQuestions/src/main/java/DecompressRunLengthEncodedList1313.java
@@ -0,0 +1,20 @@
+
+public class DecompressRunLengthEncodedList1313 {
+
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4};
+
+		decompressRLElist(array);
+	}
+	
+	public static int[] decompressRLElist(int[] nums) {
+		
+		for (int i = 0; i <= nums.length; i++) {
+			
+		}
+		
+		return nums;
+		
+	}
+
+}
